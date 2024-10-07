29966351b0ec83a34a803eb08ec8fcbaed15b1f2
igor nascimento saturnino <igornascimentosaturnino11@gmail.com>
2024-10-07 15:12:06 -0300

    Alterando para ter mais de um gerente por store

--- src/db/schema/stores.ts
+++ src/db/schema/stores.ts
@@ -10,19 +10,46 @@ export const stores = pgTable('stores', {
     .primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
-  managerId: text('manager_id').references(() => users.id, {
-    onDelete: 'set null',
-  }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
 
-export const storesRelations = relations(stores, ({ one }) => {
+// Tabela de Gerentes (associação)
+export const storeManagers = pgTable(
+  'store_managers',
+  {
+    storeId: text('store_id').references(() => stores.id, {
+      onDelete: 'cascade',
+    }),
+    managerId: text('manager_id').references(() => users.id, {
+      onDelete: 'cascade',
+    }),
+  },
+  {
+    primaryKey: ['storeId', 'managerId'],
+  },
+)
+
+// Relações
+export const storesRelations = relations(stores, ({ many }) => {
   return {
-    managers: one(users, {
-      fields: [stores.managerId],
+    managers: many(storeManagers, {
+      fields: [stores.id],
+      references: [storeManagers.storeId],
+      relationName: 'store_managers',
+    }),
+  }
+})
+
+export const storeManagersRelations = relations(storeManagers, ({ one }) => {
+  return {
+    store: one(stores, {
+      fields: [storeManagers.storeId],
+      references: [stores.id],
+    }),
+    manager: one(users, {
+      fields: [storeManagers.managerId],
       references: [users.id],
-      relationName: 'store_manager',
     }),
   }
 })
